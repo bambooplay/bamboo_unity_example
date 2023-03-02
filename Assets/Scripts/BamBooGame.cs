2a24de53ed8cdd25d41bbd633b9d7dbf202c0dd4
kuangh <kuangh@zhuziplay.com>
2023-03-02 16:07:38 +0800

    UI修改

--- Assets/Scripts/BamBooGame.cs
+++ Assets/Scripts/BamBooGame.cs
@@ -10,13 +10,16 @@ public class BamBooGame : BamBooSdkListener
 	private GameObject inputObj;
 	private GameObject outputObj;
 	private string uid = "";
+	//下拉列表中的商品ID
+	private List<string> productIdList = new List<string>();
 
 	private Dictionary<uint, long> _debugMsgDict;
 	// Start is called before the first frame update
 	void Start()
 	{
 		inputObj = GameObject.Find("Canvas/input");
-		outputObj = GameObject.Find("Canvas/output");
+		outputObj = GameObject.Find("PanelOutput");
+		setDropdownProductId();
 	}
 
 	// Update is called once per frame
@@ -29,52 +32,75 @@ public class BamBooGame : BamBooSdkListener
 	{
 		Debug.Log("exit");
 		int []intArr = new int[] {1, 2};
-		Debug.LogError("" + intArr[5]);
+		//Debug.LogError("" + intArr[5]);
 	}
 
 	public void tapInit()
 	{
-		showInput("tapInit");
+		showOutput("tapInit");
 		BamBooSdk.getInstance().setListener(this);
 		BamBooSdk.getInstance().init();
+
+		Toggle floatBallToggle = GameObject.Find("ToggleFloatBool").GetComponent<Toggle>();
+		BamBooSdk.getInstance().enableFloatBall(floatBallToggle.enabled);
+	}
+
+	public void acceptAgreement()
+	{
+		Toggle agreementToggle = GameObject.Find("ToggleAcceptAgreement").GetComponent<Toggle>();
+		BamBooSdk.getInstance().acceptAgreement(agreementToggle.enabled);
+	}
+
+	public void langedChanged(int index)
+	{
+		Debug.LogError("langedChanged " + index);
+		string[] languageArr = { "cn", "tw", "en", "thai", "indo"};
+		BamBooSdk.getInstance().setLanguage(languageArr[index]);
+	}
+
+	public void getExtendChanged(int index)
+	{
+		Debug.LogError("getExtendChanged " + index);
+		string[] extendArr = { "cpu_temperature"};
+		BamBooSdk.getInstance().getExtend(extendArr[index]);
 	}
 
 	public void tapLogin()
 	{
-		showInput("taplogin");
+		showOutput("taplogin");
 		BamBooSdk.getInstance().login();
 	}
 
 	public void tapLogout()
 	{
-		showInput("tapLogout");
+		showOutput("tapLogout");
 		BamBooSdk.getInstance().logout();
 	}
 
 
 	public void tapExit()
 	{
-		showInput("tapExit");
+		showOutput("tapExit");
 		BamBooSdk.getInstance().exit();
 	}
 
 
 	public void tapUserCenter()
 	{
-		showInput("tapUserCenter");
+		showOutput("tapUserCenter");
 		BamBooSdk.getInstance().userCenter();
 	}
 
 	public void tapGuestUpdate()
 	{
-		showInput("tapGuestUpdate");
+		showOutput("tapGuestUpdate");
 		BamBooSdk.getInstance().guestUpgrade();
 	}
 
 
 	public void tapCreateRole()
 	{
-		showInput("tapCreateRole");
+		showOutput("tapCreateRole");
 		RoleInfo roleInfo = new RoleInfo();
 		roleInfo.roleId = this.uid;
 		roleInfo.roleBalance = "0";
@@ -89,7 +115,7 @@ public class BamBooGame : BamBooSdkListener
 
 	public void tapEnterGame()
 	{
-		showInput("tapEnterGame");
+		showOutput("tapEnterGame");
 		RoleInfo roleInfo = new RoleInfo();
 		roleInfo.roleId = this.uid;
 		roleInfo.roleBalance = "0";
@@ -104,7 +130,7 @@ public class BamBooGame : BamBooSdkListener
 
 	public void tapUpdateRole()
 	{
-		showInput("tapUpdateRole");
+		showOutput("tapUpdateRole");
 		RoleInfo roleInfo = new RoleInfo();
 		roleInfo.roleId = this.uid;
 		roleInfo.roleBalance = "0";
@@ -120,26 +146,42 @@ public class BamBooGame : BamBooSdkListener
 
 	public void tapPay()
 	{
-		showInput("tapPay");
-		OrderInfo orderInfo = new OrderInfo();
-		RoleInfo roleInfo = new RoleInfo();
+		showOutput("开始支付");
+		//获取商品Id
+		Dropdown dropdown = GameObject.Find("DropdownProductId").GetComponent<Dropdown>();
+		Debug.LogError(" dropdown.value " + dropdown.value);
+		string productId = productIdList[dropdown.value];
 
-		orderInfo.cpOrderId = getOrderId();
-		orderInfo.itemId = "com.bamboogamexzsm.gp.1020";
-		orderInfo.itemName = "勾玉";
-		orderInfo.itemDesc = "10个勾玉";
-		orderInfo.extra = "extparma";
+		//获取商品价格
+		InputField inputFieldMoney = GameObject.Find("InputFieldOrderMoney").GetComponent<InputField>();
+		if (inputFieldMoney.text == null || inputFieldMoney.text == "")
+		{
+			inputFieldMoney.text = "1";
+		}
+		int money = int.Parse(inputFieldMoney.text);
+
+
+		var num = (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000;
+
+		OrderInfo orderInfo = new OrderInfo();
+		orderInfo.cpOrderId = num.ToString();
+		orderInfo.itemId = productId;
+		orderInfo.itemName = "TestProductName";
+		orderInfo.itemDesc = "TestProductDescribe";
+		orderInfo.extra = "TestExtra";
 		orderInfo.coin = 1;
-		orderInfo.money = 99;
+		orderInfo.money = money;
 		orderInfo.num = 1;
 		orderInfo.isTest = 1;
+
+		RoleInfo roleInfo = new RoleInfo();
 		roleInfo.roleId = this.uid;
 		roleInfo.roleBalance = "0";
 		roleInfo.roleLevel = "1";
-		roleInfo.roleName = "钱多多";
-		roleInfo.partyName = "同济会";
+		roleInfo.roleName = "TestRoleName";
+		roleInfo.partyName = "TestPartyName";
 		roleInfo.serverId = "1";
-		roleInfo.serverName = "火星服务器";
+		roleInfo.serverName = "TestServiceName";
 		roleInfo.vipLevel = "1";
 		BamBooSdk.getInstance().pay(orderInfo, roleInfo);
 	}
@@ -148,58 +190,58 @@ public class BamBooGame : BamBooSdkListener
 
 	public void tapBBPresetProperties()
 	{
-		showInput("tapBBPresetProperties");
+		showOutput("tapBBPresetProperties");
 		BBPresetProperties p = BamBooSdk.getInstance().getBBPresetProperties();
 		showOutputTitle("getBBPresetProperties", p);
 	}
 
 	public void tapSetBBSuperProperties()
 	{
-		showInput("tapSetBBSuperProperties");
+		showOutput("tapSetBBSuperProperties");
 		BamBooSdk.getInstance().setBBSuperProperties("{\"server_id\":\"1001\"}");
 	}
 
 	public void tapEvent()
 	{
-		showInput("tapEvent");
+		showOutput("tapEvent");
 		BamBooSdk.getInstance().taEvent("ad_event", "{\"id\":1}");
 	}
 
 	public void tapUserSet()
 	{
-		showInput("tapUserSet");
+		showOutput("tapUserSet");
 		BamBooSdk.getInstance().taUserSet("{\"id\":1}");
 	}
 
 
 	public void tapNotch()
 	{
-		showInput("tapNotch");
+		showOutput("tapNotch");
 		BamBooSdk.getInstance().getDeviceNotch();
 	}
 
 	public void tapOpenPage()
 	{
-		showInput("tapOpenPage");
+		showOutput("tapOpenPage");
 		BamBooSdk.getInstance().openPage(1, "https://www.qq.com");
 	}
 
 	public void tapMemory()
 	{
-		showInput("tapMemory");
+		showOutput("tapMemory");
 		int memory = BamBooSdk.getInstance().getProcessMemory();
 		showOutput("msg: " + memory);
 	}
 
 	public void tapRestartGame()
 	{
-		showInput("tapRestartGame");
+		showOutput("tapRestartGame");
 		BamBooSdk.getInstance().restartGame();
 	}
 
 	public void openAppStore()
 	{
-		showInput("openAppStore");
+		showOutput("openAppStore");
 		BamBooSdk.getInstance().openAppStore();
 	}
 
@@ -214,7 +256,7 @@ public class BamBooGame : BamBooSdkListener
 
 	public void tapLogReport()
 	{
-		showInput("tapLogReport");
+		showOutput("tapLogReport");
 		BamBooSdk.getInstance().logReport("info", "hiadmin");
 		BamBooSdk.getInstance().logReport("error", "error msg");
 	}
@@ -222,13 +264,13 @@ public class BamBooGame : BamBooSdkListener
 
 	public void tapGetExtend()
 	{
-		showInput("tapGetExtend");
+		showOutput("tapGetExtend");
 		BamBooSdk.getInstance().getExtend("cpu_temperature");
 	}
 
 	public void tapQuerySku()
 	{
-		showInput("tapQuerySku");
+		showOutput("tapQuerySku");
 		BamBooSdk.getInstance().querySkuDetail("com.bamboogamexzsm.gp.60,com.bamboogamexzsm.gp.5880");
 	}
 
@@ -318,7 +360,6 @@ public class BamBooGame : BamBooSdkListener
 
 	public void tapAccountInherit()
 	{
-				showInput("tapAccountInherit");
 		BamBooSdk.getInstance().accountInherit();
 	}
 
@@ -347,7 +388,7 @@ public class BamBooGame : BamBooSdkListener
 	public override void onLoginSuccess(UserInfo userInfo)
 	{
 		this.uid = userInfo.uid;
-		showOutputTitle("onLoginSuccess", userInfo);
+		showOutput("\n登录成功 \n" + Dumper.DumpAsString(userInfo) + "\n");
 	}
 
 	public override void onLoginFailed(ErrorMsg errMsg)
@@ -381,7 +422,7 @@ public class BamBooGame : BamBooSdkListener
 	public override void onSwitchAccountSuccess(UserInfo userInfo)
 	{
 		//切换账号成功，清除原来的角色信息，使用获取到新的用户信息，回到进入游戏的界面，不需要再次调登录
-		showOutputTitle("onSwitchAccountSuccess", userInfo);
+		showOutput("\n切换账号成功 \n" + Dumper.DumpAsString(userInfo) + "\n");
 	}
 
 
@@ -414,7 +455,7 @@ public class BamBooGame : BamBooSdkListener
 
 	public override void onOpenPageComplete(PageResult pageResult)
 	{
-		showOutputTitle("onOpenPageComplete", pageResult);
+		showOutput("OpenPage成功 \n" + Dumper.DumpAsString(pageResult) + "\n");
 	}
 
 	public override void onOpenPageFailed(ErrorMsg errMsg)
@@ -431,8 +472,7 @@ public class BamBooGame : BamBooSdkListener
 
 	public override void onExtendSuccess(string modName, string json)
 	{
-		showOutputTitle("onExtendSuccess", modName + ":" + json);
-		// cpu_temperature","{\"cpu_temperature\":"+tmpVal+"}"
+		showOutput("ExtendSuccess  Name：" + modName + "\n" + json + "\n");
 	}
 
 	public override void onExtendFailed(string modName, ErrorMsg msg)
@@ -442,43 +482,43 @@ public class BamBooGame : BamBooSdkListener
 
 	public override void onPaySuccess(PayResult payResult)
 	{
-		showOutputTitle("onPaySuccess", payResult);
+		showOutput("\n支付成功 \n" + Dumper.DumpAsString(payResult) + "\n");
 	}
 
 	public override void onPayFailed(PayResult payResult)
 	{
-		showOutputTitle("onPayFailed", payResult);
+		showOutput("\n支付失败 \n" + Dumper.DumpAsString(payResult) + "\n");
 	}
 
 	public override void onPayCancel(PayResult payResult)
 	{
-		showOutputTitle("onPayCancel", payResult);
+		showOutput("\n支付取消 \n" + Dumper.DumpAsString(payResult) + "\n");
 	}
 
 	public override void onNotch(NotchResult result)
 	{
-		showOutputTitle("onNotch", result);
+		showOutput("\n屏幕安全边界: \n" + Dumper.DumpAsString(result) + "\n");
 	}
 
 
 	public override void onSucceed(string infos)
 	{
-		showOutputTitle("onSucceed", infos);
+		showOutput("onSucceed: \n" + Dumper.DumpAsString(infos) + "\n");
 	}
 
 	public override void onFailed(string message)
 	{
-		showOutputTitle("onFailed", message);
+		showOutput("onFailed: \n" + Dumper.DumpAsString(message) + "\n");
 	}
 
 	public override void onSkuQueryDetailSuccess(string message)
 	{
-		showOutputTitle("onSkuQueryDetailSuccess", message);
+		showOutput("\n商品信息查询成功: \n" + Dumper.DumpAsString(message) + "\n");
 	}
 
 	public override void onSkuQueryDetailFailed( ErrorMsg msg)
 	{
-		showOutputTitle("onSkuQueryDetailFailed", msg.errMsg);
+		showOutput("商品信息查询失败: \n" + Dumper.DumpAsString(msg) + "\n");
 	}
 
 
@@ -499,23 +539,23 @@ public class BamBooGame : BamBooSdkListener
 
 	public override void onReferrer( string msg)
 	{
-		showOutputTitle("onReferrer", msg);
+		showOutput("收到归因数据: \n" + msg + "\n");
 	}
 
 	public override void onNotificationToken(string fcmToken)
 	{
-		showLog("onNotificationToken", "fcmToken: " + fcmToken);
+		showOutput("NotificationToken: " + fcmToken + "\n");
 	}
 
 	public override void onLocalNotificationClickSuccess(string notifyData)
 	{
-		showLog("onLocalNotificationClickSuccess", "notifyData: " + notifyData);
+		showOutput("收到点击通知数据: \n" + notifyData + "\n");
 	}
 
-	private string getOrderId()
+	public override void acceptAgreement(bool isAccept)
 	{
-		var num = (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000;
-		return num.ToString();
+		showOutput("acceptAgreement:" + isAccept);
+
 	}
 
 
@@ -536,12 +576,6 @@ public class BamBooGame : BamBooSdkListener
 		showLog("OnApplicationQuit", "msg: ");
 	}
 
-	void showInput(string message)
-	{
-		inputObj.GetComponent<InputField>().text = message;
-		Debug.Log(message);
-	}
-
 	void showOutputTitle(string title, object t)
 	{
 		showOutput(title + ":" + Dumper.DumpAsString(t));
@@ -549,7 +583,8 @@ public class BamBooGame : BamBooSdkListener
 
 	void showOutput(string message)
 	{
-		outputObj.GetComponent<InputField>().text = message;
+		Text outputObj = GameObject.Find("TextOutput").GetComponent<Text>();
+		outputObj.text += message += "\r\n";
 		Debug.Log(message);
 	}
 
@@ -557,5 +592,60 @@ public class BamBooGame : BamBooSdkListener
 	void showLog(string title, string message)
 	{
 		Debug.Log("title: " + title + ", message: " + message);
+	}
+
+
+	public void toggleFloatBallChange(bool change)
+	{
+		Debug.LogError("toggleFloatBallChange " + change);
+		BamBooSdk.getInstance().enableFloatBall(change);
+	}
+
+	//根据包名设置不同的商品
+	private void setDropdownProductId()
+	{
+		
+		string packageName = Application.identifier;
+		packageName = "com.bamboogame.leaf";
+		//DK项目
+		if ("com.bamboogame.leaf".Equals(packageName))
+		{
+			productIdList.Add("com.xinxiang.dk.normal.6");
+			productIdList.Add("com.xinxiang.dk.normal.648");
+			productIdList.Add("com.xinxiang.dk.normal.30");
+			productIdList.Add("com.xinxiang.dk.normal.128");
+
+		}
+		else if ("com.XJWL.SSmetaverse".Equals(packageName))
+		{
+			//炫稷项目
+			productIdList.Add("com.xjwl.ssmetaverse.9");
+			productIdList.Add("com.xjwl.ssmetaverse.7");
+			productIdList.Add("com.manhuang.tk.2");
+			productIdList.Add("com.xjwl.ssmetaverse.10");
+
+		}
+		else if ("com.bamboogame.xzthm".Equals(packageName)) {
+			//仙宗港澳台
+			productIdList.Add("com.bamboogamexzthm.gp.60");
+			productIdList.Add("com.bamboogamexzthm.gp.1280");
+			productIdList.Add("com.bamboogamexzthm.gp.2560");
+			productIdList.Add("com.bamboogamexzthm.gp.680");
+		}
+		else if ("com.bamboogame.xzsea".Equals(packageName)) {
+			//仙宗新马
+			productIdList.Add("com.bamboogamexzsm.gp.1020");
+			productIdList.Add("com.bamboogamexzsm.gp.4560");
+			productIdList.Add("com.bamboogamexzsm.gp.5880");
+			productIdList.Add("com.bamboogamexzsm.gp.monthlycard");
+		}
+		Dropdown dropdown = GameObject.Find("DropdownProductId").GetComponent<Dropdown>();
+		Dropdown.OptionData tempData;
+		for (int i = 0; i < productIdList.Count; i++)
+		{
+			tempData = new Dropdown.OptionData();
+			tempData.text = productIdList[i];
+			dropdown.options.Add(tempData);
+		}
 	}
 }
